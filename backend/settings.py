1cc729f4051c93a3adc6a2f2505d97bdc3b3e236
Ann Marusina <an.marusina2010@yandex.ru>
2025-03-31 14:48:07 +0300

    фикс статики

--- backend/settings.py
+++ backend/settings.py
@@ -60,14 +60,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend', 'staticfiles')
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
