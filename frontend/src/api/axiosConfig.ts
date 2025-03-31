7b416394a828765700c17cc1d98bd71785852960
Ann Marusina <an.marusina2010@yandex.ru>
2025-04-01 02:50:22 +0300

    фикс conf 3.0

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000", 
+  baseURL: '/api/',
+  withCredentials: true,
 });
 
 instance.interceptors.request.use((config) => {
